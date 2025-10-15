e734c919b3e38a6fbe3e206d9393d9a749bd4ef6
Zhu <jizhu@kpmg.com>
2025-10-15 19:54:00 +0100

    essentials done

--- lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
+++ lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
@@ -5,9 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import config.RewardsConfig;
+import org.springframework.jdbc.datasource.lookup.IsolationLevelDataSourceRouter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
@@ -32,5 +36,8 @@ public class SystemTestConfig {
 	//	TODO-02: Define a bean named 'transactionManager' that configures a
 	//           DataSourceTransactionManager.
 	//           How does it know which dataSource to manage?
-	
+	@Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
 }
